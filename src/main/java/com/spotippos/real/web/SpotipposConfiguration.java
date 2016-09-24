61f1705757cc381da72a03bfa448b27f6b65c63f
OC <olavocastro@gmail.com>
2016-09-24 10:59:20 -0300

    setup SpringBoot

---
+++ src/main/java/com/spotippos/real/web/SpotipposConfiguration.java
@@ -0,0 +1,12 @@
+package com.spotippos.real.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotipposConfiguration {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotipposConfiguration.class, args);
+	}
+}
