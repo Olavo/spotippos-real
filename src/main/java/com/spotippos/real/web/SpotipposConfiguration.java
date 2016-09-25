31326dec5ad8cdeae9260bec335973f2531c4f80
OC <olavocastro@gmail.com>
2016-09-25 12:23:33 -0300

    carga dos imoveis.

--- src/main/java/com/spotippos/real/web/SpotipposConfiguration.java
+++ src/main/java/com/spotippos/real/web/SpotipposConfiguration.java
@@ -1,12 +1,48 @@
 package com.spotippos.real.web;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.File;
+import java.io.IOException;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spotippos.real.dominio.model.Imoveis;
+
+@EnableAutoConfiguration
+@ComponentScan(basePackages = { "com.spotippos.real" })
 @SpringBootApplication
 public class SpotipposConfiguration {
 
+	@Value("${pasta.configuracao.json.imoveis}")
+	String nomeArquivoImoveis;
+
+	private Resource resource;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpotipposConfiguration.class, args);
+	}
+
+	@PostConstruct
+	public void setUp() throws IOException {
+		resource = new ClassPathResource(nomeArquivoImoveis);
+	}
+
+	@Bean(name = "imoveis")
+	public Imoveis getArquivoImoveis() throws IOException {
+
+		ObjectMapper jsonMapper = new ObjectMapper();
+		File jsonFile = resource.getFile();
+		return jsonMapper.readValue(jsonFile, new TypeReference<Imoveis>() {});
+
 	}
 }
