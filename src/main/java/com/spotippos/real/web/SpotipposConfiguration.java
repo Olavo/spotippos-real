4557b0e1194fc7982a96526c44713d52485df17c
OC <olavocastro@gmail.com>
2016-09-25 22:53:51 -0300

    carga de provincies.

--- src/main/java/com/spotippos/real/web/SpotipposConfiguration.java
+++ src/main/java/com/spotippos/real/web/SpotipposConfiguration.java
@@ -2,8 +2,7 @@ package com.spotippos.real.web;
 
 import java.io.File;
 import java.io.IOException;
-
-import javax.annotation.PostConstruct;
+import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -16,15 +15,19 @@ import org.springframework.core.io.Resource;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spotippos.real.dominio.model.Boundary;
 import com.spotippos.real.dominio.model.Imoveis;
 
 @EnableAutoConfiguration
-@ComponentScan(basePackages = { "com.spotippos.real" })
+@ComponentScan(basePackages = { "com.spotippos.real.*" })
 @SpringBootApplication
 public class SpotipposConfiguration {
 
 	@Value("${pasta.configuracao.json.imoveis}")
-	String nomeArquivoImoveis;
+	private String nomeArquivoImoveis;
+
+	@Value("${pasta.configuracao.json.provinces}")
+	private String nomeArquivoProvinces;
 
 	private Resource resource;
 
@@ -32,17 +35,26 @@ public class SpotipposConfiguration {
 		SpringApplication.run(SpotipposConfiguration.class, args);
 	}
 
-	@PostConstruct
-	public void setUp() throws IOException {
-		resource = new ClassPathResource(nomeArquivoImoveis);
-	}
-
 	@Bean(name = "imoveis")
 	public Imoveis getArquivoImoveis() throws IOException {
-
-		ObjectMapper jsonMapper = new ObjectMapper();
+		resource = new ClassPathResource(nomeArquivoImoveis);
 		File jsonFile = resource.getFile();
-		return jsonMapper.readValue(jsonFile, new TypeReference<Imoveis>() {});
+		ObjectMapper jsonMapper = new ObjectMapper();
+		return jsonMapper.readValue(jsonFile, new TypeReference<Imoveis>() {
+		});
 
 	}
+
+	@Bean(name = "provinces")
+	public HashMap<String, Boundary> getArquivoProvinces() throws IOException {
+		resource = new ClassPathResource(nomeArquivoProvinces);
+		File jsonFile = resource.getFile();
+		ObjectMapper jsonMapper = new ObjectMapper();
+
+		HashMap<String, Boundary> provinces =jsonMapper.readValue(jsonFile, HashMap.class);
+		
+		return provinces;
+
+	}
+
 }
